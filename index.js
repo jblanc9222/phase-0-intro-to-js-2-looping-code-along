ff22a1d42aca7a6ffe4fdf94dc297f9654e44fa8
Joe <joeleblanc920@gmail.com>
2021-11-01 20:43:56 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const cards= ["Lisa","Kaitlin","Jan"];
+
+    function writeCards() {
+        const phrase=[]
+      for (let i = 0; i <cards.length; i++) {
+        phrase.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+                    }
+           
+            return phrase;                           
+        } 
+    
+        
+      
+    writeCards(["Lisa","Kaitlin","Jan"]);
+
+    function countDown(integer){
+        while(integer>=0){
+            console.log(integer);
+            integer--;
+
+        }
+    }
+   
